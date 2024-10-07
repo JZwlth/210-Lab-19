683e2b74c9e327cc235bf6a7d59fca9f69d024c8
JZwlth <wkjazy87@gmail.com>
2024-10-06 21:00:11 -0700

    update

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
+// COMSC-210 | Lab 19 | Zhaoyi Zheng
+
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
+
+// Function prototypes
+
+
+class Movie {
+private:
+    string title;
 
 int main () {
     cout << "hihi";
